ee0665af5a3f609b591f3f690944c17087b62a45
nmpereira <nikhilpereira25@gmail.com>
2021-04-06 21:06:39 -0400

    Update to FV.py

--- src/FV.py
+++ src/FV.py
@@ -1,27 +1,41 @@
 import numpy_financial as npf
 from statistics import mean
 from array import *
+import FV
 
 x= []
-nperlist = array('i',[])
+
+
+
+rate = int(input("Enter Rate: "))
+listOfN = array('i',[])
+pmt= int(input("Enter PMT: "))
+pv = int(input("Enter PV: "))
 
 n = int(input("Number of entries: "))
 for i in range(n):
-    L = int(input("Enter a number: "))
-    nperlist.append(L)
+    L = int(input("Enter the periods for each entry: "))
+    listOfN.append(L)
 
 
 def future_value(rate, nper, pmt, pv):  
     global y 
     y= npf.fv(rate, nper, pmt, pv)
-
-for i in range(10):
-    for nper in nperlist:
-        future_value(0.05/12, nper*12, -100, -100)
-        x.append(y)
+    
 
 
-print(mean(x))
+for nper in listOfN:
+    future_value((rate/100), nper, pmt, pv)
+    x.append(y)
 
-input()
+print(x)
+if n>1:
+    print('mean: ', mean(x))
+
+#callback same file to repeat another calc
+print(" ")
+print(" ")
+FV
+
+input("Press enter to start another calculation")
 
