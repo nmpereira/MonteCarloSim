a410dfadb2c11c90b7870a17e68d16063e95b40e
nmpereira <nikhilpereira25@gmail.com>
2021-04-07 02:21:37 -0400

    Update FV.py to add dataframe and support for large decimals

--- src/FV.py
+++ src/FV.py
@@ -1,16 +1,23 @@
+from decimal import Decimal
 import numpy_financial as npf
 from statistics import mean
 from array import *
 import FV
+import numpy as np
 
 x= []
 
+# rate = int(input("Enter Rate: "))
+# pmt= int(input("Enter PMT: "))
+# pv = int(input("Enter PV: "))
+# listOfN = array('i',[])
 
-
-rate = int(input("Enter Rate: "))
+rate = 5
+pmt= 1000
+pv = 10000
 listOfN = array('i',[])
-pmt= int(input("Enter PMT: "))
-pv = int(input("Enter PV: "))
+
+
 
 n = int(input("Number of entries: "))
 for i in range(n):
@@ -19,18 +26,28 @@ for i in range(n):
 
 
 def future_value(rate, nper, pmt, pv):  
-    global y 
-    y= npf.fv(rate, nper, pmt, pv)
+    global y
+    #y = int()
+    y = Decimal(npf.fv(rate, nper, pmt, pv))
+    y = (round(y, 2))
     
 
 
 for nper in listOfN:
     future_value((rate/100), nper, pmt, pv)
     x.append(y)
+    
 
-print(x)
+
+
+print(np.matrix(x))
+
+import pandas as pd
+pd.set_option("max_colwidth", None)
+print(pd.DataFrame(x))
+
 if n>1:
-    print('mean: ', mean(x))
+    print('mean: ', round(mean(x), 2))
 
 #callback same file to repeat another calc
 print(" ")
