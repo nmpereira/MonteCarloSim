d11568b953b942644c0050d2cc3f418dddef5de6
npereira <nikhilpereira25@gmail.com>
2021-07-12 17:11:12 -0400

    Update MontecarloInterest.py

--- src/MonteCarloInterest.py
+++ src/MonteCarloInterest.py
@@ -4,6 +4,7 @@ from tabulate import tabulate
 import pandas as pd
 import numpy as np
 import numpy_financial as npf
+import statistics
 
 
 variables_imported = [
@@ -23,123 +24,136 @@ variables_imported = [
 print(tabulate(variables_imported))
 num_years = Age_end - current_age
 Grand_Future_Value=[]
-def simulate():
-    index=[]
-    nper=[]
-    data=[]
-    PresentValue=[]
-    AnnualRate=[]
-    PaymentAmount=[]
-    FutreValue=[]
-    
-
-    def randomizer(lower, upper):
-        return round(random.uniform(lower, upper), 2)
-
-    #def num_years():
-    #    pass
-
-    def indexer(num_years):
-        for years in range(num_years):
-            index.append(years)
-        data.append(index)
-        return index
-            
-
-    def rate(return_lower, return_upper):
-        return randomizer(return_lower, return_upper)
-
-    def Nperiod(index):
-        for i in range(index):
-            nper.append(i+1)
-        data.append(nper)
-        return nper
-
-    def pmt(amount_lower, amount_upper):
-        return randomizer(amount_lower, amount_upper)
-
-    def present_value(index):
-        for i in index:
-            PresentValue.append(0)
-        PresentValue[0]= pv
-        data.append(PresentValue)
-        return PresentValue
-    def Annual_rate(index):
-        for i in index:
-            AnnualRate.append(rate(return_lower, return_upper))
-        data.append(AnnualRate)
-        return AnnualRate
-    def Payment_amount(index):
-        for i in index:
-            PaymentAmount.append(pmt(amount_lower, amount_upper))
-        data.append(PaymentAmount)
-        return PaymentAmount
-
-    def Future_value(index):
-        def futureval(AnnualRate,index,PaymentAmount, PresentValue):
-            return round(npf.fv(AnnualRate, index, PaymentAmount, PresentValue),2)
-
-        for i in index:
-            if i ==0:
-                FutreValue.append(futureval(AnnualRate[i]/100,nper[i],PaymentAmount[i], PresentValue[i]))
-                
-            else:
-                FutreValue.append(futureval(AnnualRate[i]/100,nper[i],PaymentAmount[i], PresentValue[i]+PresentValue[0]))
-                
-        data.append(FutreValue)
-        return FutreValue
+Grand_Future_Value_mean=[]
 
 
-
-
-    dataframe = []
-    #dataframe.clear()
-       
-    pmt(amount_lower, amount_upper)
-    indexer(num_years)
-    Nperiod(num_years)
-    present_value(index)
-    Annual_rate(index)
-    Payment_amount(index)
-    Future_value(index)
-
-
-
-    # print("rate:",return_lower, return_upper)
-    # print("pmt:",pmt(amount_lower, amount_upper))
-    # print("num_years:",num_years)
-
-    # print("indexer:",indexer(num_years))
-    # print("Nperiod:",Nperiod(num_years))
-    # print("present_value:", present_value(index))
-    # print("Annual_rate:",Annual_rate(index))
-    # print("Payment_amount:",Payment_amount(index))
-    # print("Future_value:",Future_value(index))
-
-    pd.set_option("max_colwidth", None)
-    dataframe = pd.DataFrame(data).transpose()
-    dataframe.columns = ["index","Year", "Present Value", "rate","Payment Amount", "Future Value"]
-    format_mapping = {"Payment Amount": "${:,.2f}", "Year": "{:,.0f}", "rate": "{:.2f}%"}
-
-    
-    dataframe['index']=dataframe['index'].map('{:,.0f}'.format)
-    dataframe['Year']=dataframe['Year'].map('{:,.0f}'.format)
-
-    dataframe['rate']=dataframe['rate'].map('{:.3f}%'.format)
-    dataframe['Payment Amount']=dataframe['Payment Amount'].map('$ {:,}'.format)
-    dataframe['Present Value']=dataframe['Present Value'].map('$ {:,}'.format)
-    dataframe['Future Value']=dataframe['Future Value'].map('$ {:,}'.format)
-
-    #print("pd.DataFrame(dataframe) \n", dataframe)
-
-    Grand_Future_Value.append(dataframe['Future Value'].iloc[-1])
-
-
-def simulation(trials):
+def simulate(trials):
     for trial in range(trials):
-        simulate()
+        index=[]
+        nper=[]
+        data=[]
+        PresentValue=[]
+        AnnualRate=[]
+        PaymentAmount=[]
+        FutreValue=[]
+        
+        
 
-simulation(1000)
+        def randomizer(lower, upper):
+            return round(random.uniform(lower, upper), 2)
+
+        #def num_years():
+        #    pass
+
+        def indexer(num_years):
+            for years in range(num_years):
+                index.append(years)
+            data.append(index)
+            return index
+                
+
+        def rate(return_lower, return_upper):
+            return randomizer(return_lower, return_upper)
+
+        def Nperiod(index):
+            for i in range(index):
+                nper.append(i+1)
+            data.append(nper)
+            return nper
+
+        def pmt(amount_lower, amount_upper):
+            return randomizer(amount_lower, amount_upper)
+
+        def present_value(index):
+            for i in index:
+                PresentValue.append(0)
+            PresentValue[0]= pv
+            data.append(PresentValue)
+            return PresentValue
+        def Annual_rate(index):
+            for i in index:
+                AnnualRate.append(rate(return_lower, return_upper))
+            data.append(AnnualRate)
+            return AnnualRate
+        def Payment_amount(index):
+            for i in index:
+                PaymentAmount.append(pmt(amount_lower, amount_upper))
+            data.append(PaymentAmount)
+            return PaymentAmount
+
+        def Future_value(index):
+            def futureval(AnnualRate,index,PaymentAmount, PresentValue):
+                return round(npf.fv(AnnualRate, index, PaymentAmount, PresentValue),2)
+
+                
+
+            for i in index:
+                if i ==0:
+                    FutreValue.append(futureval(AnnualRate[i]/100,nper[i],PaymentAmount[i], PresentValue[i]))
+                    
+                else:
+                    FutreValue.append(futureval(AnnualRate[i]/100,nper[i],PaymentAmount[i], PresentValue[i]+PresentValue[0]))
+                    
+            data.append(FutreValue)
+            return FutreValue
+
+
+
+
+        dataframe = []
+        #dataframe.clear()
+        
+        pmt(amount_lower, amount_upper)
+        indexer(num_years)
+        Nperiod(num_years)
+        present_value(index)
+        Annual_rate(index)
+        Payment_amount(index)
+        Future_value(index)
+
+
+
+        # print("rate:",return_lower, return_upper)
+        # print("pmt:",pmt(amount_lower, amount_upper))
+        # print("num_years:",num_years)
+
+        # print("indexer:",indexer(num_years))
+        # print("Nperiod:",Nperiod(num_years))
+        # print("present_value:", present_value(index))
+        # print("Annual_rate:",Annual_rate(index))
+        # print("Payment_amount:",Payment_amount(index))
+        # print("Future_value:",Future_value(index))
+
+        pd.set_option("max_colwidth", None)
+        dataframe = pd.DataFrame(data).transpose()
+        dataframe.columns = ["index","Year", "Present Value", "rate","Payment Amount", "Future Value"]
+
+
+        
+        dataframe['index']=dataframe['index'].map('{:,.0f}'.format)
+        dataframe['Year']=dataframe['Year'].map('{:,.0f}'.format)
+
+        dataframe['rate']=dataframe['rate'].map('{:.3f} %'.format)
+        dataframe['Payment Amount']=dataframe['Payment Amount'].map('$ {:,}'.format)
+        dataframe['Present Value']=dataframe['Present Value'].map('$ {:,}'.format)
+
+        #print("pd.DataFrame(dataframe) \n", dataframe)
+        
+        Grand_Future_Value.append(dataframe['Future Value'].iloc[-1])
+        
+
+        dataframe['Future Value']=dataframe['Future Value'].map('$ {:,}'.format)
+        if trial==0:
+            print("pd.DataFrame(dataframe) \n", dataframe)
+
+simulate(6000)
+pd.set_option('display.float_format', '$ {:,}'.format)
 Grand_dataframe = pd.DataFrame(Grand_Future_Value)
-print("Grand_dataframe",Grand_dataframe)
+Grand_dataframe.columns = ["Trials"]
+print("Grand_dataframe \n",Grand_dataframe)
+Grand_Future_Value_mean=statistics.mean(Grand_Future_Value)
+print( "Average: ",'$ {:,}'.format(round(Grand_Future_Value_mean,2)))
 
+print(Grand_dataframe.describe())
+
+input("Press Enter to continue.")
--- src/Variables.py
+++ src/Variables.py
@@ -3,10 +3,10 @@
 pv = 50000
 #num_years = 10
 #L=40
-return_lower =0
-return_upper=10
-amount_lower =12000
-amount_upper=12000
+return_lower =1
+return_upper=30
+amount_lower =1*12
+amount_upper=2000*12
 current_age= 25
 Age_end=65
 simulation_trials= 0
